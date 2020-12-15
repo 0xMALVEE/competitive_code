e58f036b76b2ebdabfb99eec4f4975f94be4d301
Your Name <you@example.com>
2020-12-15 22:22:36 +0600

    DONE

---
+++ Codeforces/B_Last_Year_s_Substring.cpp
@@ -0,0 +1,110 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+ 
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+
+ 
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001; //check the limits, dummy
+ 
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);    
+	
+    ll T; cin >> T;
+    while(T--) {
+        ll n ;
+        cin >> n;
+        string k;
+        cin >> k;
+
+        ll run = 1;
+        ll result = 0;
+
+        if(n < 4){
+            cout << "NO" << endl;
+            continue;
+        }
+
+        if(k == "2020"){
+            cout << "YES" << endl;
+            continue;
+           
+        }else{
+            for(ll i = 0;i< n;i++){
+
+                for(ll j =0;j< n;j++){
+                    if(i >= 1 && j <= n && i < j && run  == 1){
+                        string temp = k;
+                        
+                        k.replace(i,j ,"");
+                        if(k == "2020"){
+                            cout << "YES" << endl;
+                            
+                            run = 0;
+                            result = 0;
+                            continue;
+                            continue;
+                            continue;
+                           
+                        }else{
+                            k = temp;
+                            result  = 1;
+                        }
+                        
+                        // cout << k << endl;
+                       
+                    }
+                    
+                }
+            }
+        }
+       if(result == 1){
+        cout << "NO" << endl;
+       }
+        
+    }
+	
+	return 0;
+}
+ 
+// read the question correctly (ll vs int)
+// template by bqi343
